d622f4dfd86bfa697968c3662a45703a8a1d437f
sobelz <117825179+sobelz@users.noreply.github.com>
2024-05-06 14:29:08 +0330

    deleted onload

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-window.addEventListener("load", function () {
-  ReactDOM.createRoot(document.getElementById('MapWrapper')).render(
-    <App />,
-  )
-})
+ReactDOM.createRoot(document.getElementById('MapWrapper')).render(
+  <App />,
+)
